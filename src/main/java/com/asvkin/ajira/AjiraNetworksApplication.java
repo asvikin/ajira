a21b63b0ea25e7a21fc14b11c626131ba19f726a
asvikin <vignesh.subramanian@tatacommunications.com>
2020-12-15 21:31:20 +0530

    First commit

---
+++ src/main/java/com/asvkin/ajira/AjiraNetworksApplication.java
@@ -0,0 +1,13 @@
+package com.asvkin.ajira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjiraNetworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjiraNetworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asvkin/ajira/AjiraNetworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asvkin.ajira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjiraNetworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
